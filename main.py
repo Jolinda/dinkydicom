55075d916a2ae649be549295db81ce92cbbda47b
jolinda <jolinda@uoregon.edu>
2023-12-19 15:22:31 -0800

    added filename title

--- main.py
+++ main.py
@@ -7,56 +7,24 @@ import matplotlib
 import PySimpleGUI as sg
 import pydicom
 
-
-def ShowSeries(foldername):
+def open_series(foldername):
     folderpath = pathlib.Path(foldername)
     if not folderpath.exists():
         sg.popup_error('Folder not found', no_titlebar=True)
         return None
 
-    allfiles = [pydicom.dcmread(x) for x in folderpath.glob('*.dcm')]
+    allfiles = [(x, pydicom.dcmread(x)) for x in folderpath.glob('*.dcm')]
 
     if not allfiles:
         sg.popup_error('No dicoms in folder', no_titlebar=True)
         return None, None
 
-    allfiles.sort(key=lambda x: int(x['InstanceNumber'].value))
-    allfiles = [x for x in allfiles if x.pixel_array.shape == allfiles[0].pixel_array.shape]
-    if len(allfiles[0].pixel_array.shape) > 2:
-        alldata = np.array([mosaify(x.pixel_array) for x in allfiles])
-    else:
-        alldata = np.array([x.pixel_array for x in allfiles])
-
-    print(alldata.shape)
-    return allfiles, alldata
-def GetFigure(data):
-    fig, ax = plt.subplots()
-#    ax.set_title(filename.name)
-    ax.imshow(data, cmap='gray')
-    ax.set_axis_off()
-    return fig
-
-def ShowDicom(filename):
-    if not filename.exists():
-        sg.popup_error('File not found', no_titlebar=True)
-        return None
-
-    img = pydicom.dcmread(filename)
-    print(filename)
-    print(img['InstanceNumber'])
-    if len(img.pixel_array.shape) > 2:
-        data = mosaify(img.pixel_array)
-    else:
-        data = img.pixel_array
-
-    fig, ax = plt.subplots()
-    ax.set_title(filename.name)
-    ax.imshow(data, cmap='gray')
-    ax.set_axis_off()
-    return fig
-
+    allfiles.sort(key=lambda x: int(x[1]['InstanceNumber'].value))
+    return allfiles
 
 def mosaify(data):
+    if len(data.shape) < 3:
+        return data
     nacross = int(np.floor(np.sqrt(data.shape[0])))
     ndown = int(np.ceil(data.shape[0] / nacross))
     mdata = data.copy()
@@ -65,91 +33,55 @@ def mosaify(data):
         .reshape(mdata.shape[1], ndown, -1).reshape((int(mdata.shape[1]*ndown), -1), order='F')
     return mdata
 
-# from https://www.tutorialspoint.com/pysimplegui/pysimplegui_matplotlib_integration.htm
 def draw_figure(canvas, figure):
    tkcanvas = FigureCanvasTkAgg(figure, canvas)
    tkcanvas.draw()
    tkcanvas.get_tk_widget().pack(side='top', fill='both', expand=1)
    return tkcanvas
 
-def redraw_figure(canvas, data):
-   plt.clf()
-   plt.imshow(data, cmap='gray')
-   tkcanvas = FigureCanvasTkAgg(figure, canvas)
-   tkcanvas.draw()
-   tkcanvas.get_tk_widget().pack(side='top', fill='both', expand=1)
-   return tkcanvas
-
-def pack_figure(graph, figure):
-    canvas = FigureCanvasTkAgg(figure, graph.Widget)
-    plot_widget = canvas.get_tk_widget()
-    plot_widget.pack(side='top', fill='both', expand=1)
-    return plot_widget
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     dicom_path = pathlib.Path('/projects/lcni/dcm/lcni/Smith/xa30_test')
     sg.theme('Dark Blue 3')
 
     layout = [[sg.Text('Dicom folder')],
-              [sg.Input(key='dicom', size=(120, None)),
-               sg.FolderBrowse(key='dicom_browser', initial_folder=dicom_path)],
-              [sg.Button('Plot'), sg.Spin([0], key='Frame', enable_events=True), sg.Button('Exit')],
+              [sg.Input(key='dicom', size=(120, None), enable_events=True),
+               sg.FolderBrowse(key='browser', initial_folder=dicom_path)],
+              [sg.Text('Frame'), sg.Spin([0], key='frame', enable_events=True), sg.Button('Exit')],
               [sg.Canvas(key='canvas', expand_x=True, expand_y=True)]
-              #[sg.Graph((640,480), (0,0), (640,480), key='Graph')]
               ]
 
-    # Create the Window
     window = sg.Window('Multiframe DICOM viewer', layout, resizable=True, finalize=True)
-    # Event Loop to process "events" and get the "values" of the inputs
-    #figure_canvas = None
-    #graph = window['Graph']
 
     fig,ax = plt.subplots()
     ax.set_axis_off()
     draw_figure(window['canvas'].TKCanvas, fig)
-    #pack_figure(graph, fig)
+    dicom_data = None
 
     while True:
         event, values = window.read()
         #print(event, values)
         if event in (None, 'Exit'):  # if user closes window or clicks Exit
             break
-        if event == 'Plot':
-            #if figure_canvas:
-            #   figure_canvas.get_tk_widget().forget()
-            #   plt.close('all')
-            frames, data = ShowSeries(values['dicom'])
-
-
-            if frames:
-                assert(len(frames) == data.shape[0])
-            #    figure = GetFigure(data[0,:,:])
-                window.Element('Frame').Update(values=[x for x in range(0, len(frames))], value=0)
-                plt.clf()
-                plt.imshow(data[0, :, :], cmap='gray')
-                fig.canvas.draw()
-            #    figure_canvas = draw_figure(window['canvas'].TKCanvas, figure)
-            #else:
-            #    window.Element('Frame').Update(values=[0], value=0)
-            #dicom_figure = ShowSeries(values['dicom'])
-            #if figure_canvas:
-            #    figure_canvas.get_tk_widget().forget()
-            #    plt.close('all')
-            #figure_canvas = draw_figure(window['canvas'].TKCanvas, dicom_figure)
-
-        if event=='Frame': # and figure_canvas:
-            #figure_canvas.get_tk_widget().forget()
-            #plt.close('all')
-            frameNo = values['Frame']
-            plt.clf()
-            plt.imshow(data[frameNo, :, :], cmap='gray')
+        if event == 'dicom':
+            dicom_data = open_series(values['dicom'])
+            ax.clear()
+            ax.set_axis_off()
+            if dicom_data:
+                window.Element('frame').Update(values=[x for x in range(0, len(dicom_data))], value=0)
+                ax.set_title(dicom_data[0][0].name)
+                ax.imshow(mosaify(dicom_data[0][1].pixel_array), cmap='gray')
+            else:
+                window.Element('frame').Update(values=[0], value=0)
             fig.canvas.draw()
-            #print(frameNo)
-            #figure_canvas = draw_figure(window['canvas'].TKCanvas, figs[frameNo])
-            #figure_canvas = draw_figure(window['canvas'].TKCanvas, GetFigure(data[frameNo,:,:]))
-            #figure_canvas = redraw_figure(window['canvas'].TKCanvas, data[frameNo, :, :])
+
+        if event=='frame' and dicom_data:
+            frameNo = values['frame']
+            ax.clear()
+            ax.set_title(dicom_data[frameNo][0].name)
+            ax.set_axis_off()
+            ax.imshow(mosaify(dicom_data[frameNo][1].pixel_array), cmap='gray')
+            fig.canvas.draw()
 
     window.close()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
